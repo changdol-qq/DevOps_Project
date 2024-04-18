abba5bf49af1d6d17e52b30207677ee631b11ce6
changdol-qq <97380181+changdol-qq@users.noreply.github.com>
2024-04-18 13:28:31 +0900

    file relocation

--- Python_Web_Application/market/__init__.py
+++ Python_Web_Application/market/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '250786e4fb334f739c87706c'
 db = SQLAlchemy(app)
 
 from .models import Item
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 from market import routes
 
