86a8fbe45bd15cd850d84fc4509d500eb400f1eb
changdol-qq <97380181+changdol-qq@users.noreply.github.com>
2024-04-19 13:59:24 +0900

    add User Authentication function in web_app

--- Python_Web_Application/market/__init__.py
+++ Python_Web_Application/market/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '250786e4fb334f739c87706c'
 db = SQLAlchemy(app)
-
-from .models import Item
-# with app.app_context():
-#     db.create_all()
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
 
--- Python_Web_Application/market/routes.py
+++ Python_Web_Application/market/routes.py
@@ -1,17 +1,19 @@
 from market import app, db
 from market.models import Item, User
 from flask import render_template, redirect, url_for, flash
-from market.forms import RegisterFrom
+from market.forms import RegisterFrom, LoginForm
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home/')
 def home_page():
     return render_template('home.html')
+
 with app.app_context():
     db.create_all()
+
 @app.route('/market')
 def market_page():
-
     items = Item.query.all()
     return render_template('market.html', items=items)
 
@@ -21,7 +23,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash= form.password1.data)
+                              password = form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -29,3 +31,19 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'There was an error with createing a user:{err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form =LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category= 'danger')
+
+    return render_template('login.html', form=form)
