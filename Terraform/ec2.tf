ef33b1a8057b90e164ff4e6fdfff8e23469af7c2
changdol-qq <97380181+changdol-qq@users.noreply.github.com>
2024-04-25 17:40:28 +0900

    modified ec2.tf file

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,20 +1,38 @@
-resource "aws_instance" "app-server-1" {
+resource "aws_instance" "Control_plane" {
   ami           = "ami-0d3d9b94632ba1e57"
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.security_group_1.id]
   # aws_security_group 사용
   user_data = <<-EOF
     #!/bin/bash
-    apt-get update
+    sudo apt-get update
+    sudo hostnamectl set-hostname control-plane
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
 EOF
   tags = {
-    Name = "App"
+    Name = "control"
   }
 }
-
-
-resource "aws_db_instance" "default" {
-  instance_class = "db.t2.micro"
-  engine         = "mysql"
-  allocated_storage = 20
+/*
+Change the hostname
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+*/
+resource "aws_instace" "Worker_node" {
+  ami           = 
+  instance_type = "t2.micro"
+  count         =  2
+  vpc_security_gorup_ids = 
+  tags = "worker"
 }
+
+
+
+# resource "aws_db_instance" "default" {
+#   instance_class = "db.t2.micro"
+#   engine         = "mysql"
+#   allocated_storage = 20
+# }
