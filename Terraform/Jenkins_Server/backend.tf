0f06c6a027d3fb2569fe2ae98c91ce7b93d0ce37
changdol-qq <97380181+changdol-qq@users.noreply.github.com>
2024-05-14 22:55:27 +0900

    organized folder and file

--- Terraform/Jenkins_Server/backend.tf
+++ Terraform/Jenkins_Server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mark-cicd-terraform-eks" #이미 생성 되어있는 버켓 사용??
+    bucket = "mark-cicd-terraform-eks" 
     key    = "jenkins/terraform.tfstate"
     region = "ap-northeast-2"
   }
--- Terraform/output.tf
+++
@@ -1,8 +0,0 @@
-output "web_ip" {
-  value = aws_instance.app-server-1.public_ip 
-}
-
-output "alb_dns_name" {
-  value = aws_lb.lb-1
-  description = "The domain name of the load balancer"
-}
